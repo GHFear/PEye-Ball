f429fddbeecf5bb294d734a1e4250c989599df13
GHFear (Adam J) <32522043+GHFear@users.noreply.github.com>
2023-10-24 23:01:00 +0200

    Add files via upload

--- PEye-Ball/includes/lambdas.h
+++ PEye-Ball/includes/lambdas.h
@@ -1,7 +1,9 @@
 #pragma once
+
+#ifdef __linux__
+
 bool start_pe_parser(PE_DATABASE* database, void* exe_base, int loadFile)
 {
-    #ifdef __linux__
 	auto dos_lambda = [](PE_DATABASE* database, void* exe_base, int loadFile) {
 		if (!create_dos_header(database, exe_base)) { clean_exit(loadFile); }
 		};
@@ -36,7 +38,34 @@ bool start_pe_parser(PE_DATABASE* database, void* exe_base, int loadFile)
 		if (!print_delayed_import_descriptors(database, exe_base)) { clean_exit(loadFile); }
 		};
 
-	#elif _WIN64
+	//Build DOS -> Sections
+	std::thread dos_thread(dos_lambda, database, exe_base, loadFile);
+	std::thread nt_thread(nt_lambda, database, exe_base, loadFile);
+	std::thread section_thread(section_lambda, database, exe_base, loadFile);
+	dos_thread.join();
+	nt_thread.join();
+	section_thread.join();
+
+	//Build DataDirectories
+	std::thread import_thread(import_lambda, database, exe_base, loadFile);
+	std::thread export_thread(export_lambda, database, exe_base, loadFile);
+	std::thread delayed_import_thread(delayed_import_lambda, database, exe_base, loadFile);
+	import_thread.join();
+	export_thread.join();
+	delayed_import_thread.join();
+
+	//Print Database
+	std::thread print_thread(print_lambda, database, exe_base, loadFile);
+	print_thread.join();
+
+    return true;
+}
+
+#elif _WIN64
+
+bool start_pe_parser(PE_DATABASE* database, void* exe_base, HANDLE loadFile)
+{
+
 	auto dos_lambda = [](PE_DATABASE* database, void* exe_base, HANDLE loadFile) {
 		if (!create_dos_header(database, exe_base)) { clean_exit(loadFile); }
 		};
@@ -70,7 +99,7 @@ bool start_pe_parser(PE_DATABASE* database, void* exe_base, int loadFile)
 		if (!print_export_functions(database, exe_base)) { clean_exit(loadFile); }
 		if (!print_delayed_import_descriptors(database, exe_base)) { clean_exit(loadFile); }
 		};
-	#endif
+
 
 	//Build DOS -> Sections
 	std::thread dos_thread(dos_lambda, database, exe_base, loadFile);
@@ -92,5 +121,7 @@ bool start_pe_parser(PE_DATABASE* database, void* exe_base, int loadFile)
 	std::thread print_thread(print_lambda, database, exe_base, loadFile);
 	print_thread.join();
 
-    return true;
+	return true;
 }
+
+#endif
